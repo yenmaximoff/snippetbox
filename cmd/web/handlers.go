6ed517130516363e9f18451aa56b7dc9c80b3617
yenmaximoff <118884356+yenmaximoff@users.noreply.github.com>
2023-10-18 18:16:45 +0500

    snippetbox
    snippetbox

---
+++ cmd/web/handlers.go
@@ -0,0 +1,74 @@
+package main
+
+import (
+    "errors"
+    "fmt"
+    "golangify.com/snippetbox/pkg/models"
+    "net/http"
+    "strconv"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/" {
+        app.notFound(w)
+        return
+    }
+
+    s, err := app.snippets.Latest()
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    // Используем помощника render() для отображения шаблона.
+    app.render(w, r, "home.page.tmpl", &templateData{
+        Snippets: s,
+    })
+}
+
+func (app *application) showSnippet(w http.ResponseWriter, r *http.Request) {
+    id, err := strconv.Atoi(r.URL.Query().Get("id"))
+    if err != nil || id < 1 {
+        app.notFound(w)
+        return
+    }
+
+    s, err := app.snippets.Get(id)
+    if err != nil {
+        if errors.Is(err, models.ErrNoRecord) {
+            app.notFound(w)
+        } else {
+            app.serverError(w, err)
+        }
+        return
+    }
+
+    // Используем помощника render() для отображения шаблона.
+    app.render(w, r, "show.page.tmpl", &templateData{
+        Snippet: s,
+    })
+}
+
+func (app *application) createSnippet(w http.ResponseWriter, r *http.Request) {
+    if r.Method != http.MethodPost {
+        w.Header().Set("Allow", http.MethodPost)
+        app.clientError(w, http.StatusMethodNotAllowed)
+        return
+    }
+
+    // Создаем несколько переменных, содержащих тестовые данные. Мы удалим их позже.
+    title := "История про улитку"
+    content := "Улитка выползла из раковины,\nвытянула рожки,\nи опять подобрала их."
+    expires := "7"
+
+    // Передаем данные в метод SnippetModel.Insert(), получая обратно
+    // ID только что созданной записи в базу данных.
+    id, err := app.snippets.Insert(title, content, expires)
+    if err != nil {
+        app.serverError(w, err)
+        return
+    }
+
+    // Перенаправляем пользователя на соответствующую страницу заметки.
+    http.Redirect(w, r, fmt.Sprintf("/snippet?id=%d", id), http.StatusSeeOther)
+}
---
+++ cmd/web/main.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+    "database/sql"
+    "flag"
+    "html/template" // Новый импорт
+    "log"
+    "net/http"
+    "os"
+
+    _ "github.com/go-sql-driver/mysql"
+    "golangify.com/snippetbox/pkg/models/mysql"
+)
+
+// Добавляем поле templateCache в структуру зависимостей. Это позволит
+// получить доступ к кэшу во всех обработчиках.
+type application struct {
+    errorLog      *log.Logger
+    infoLog       *log.Logger
+    snippets      *mysql.SnippetModel
+    templateCache map[string]*template.Template
+}
+
+func main() {
+    addr := flag.String("addr", ":4000", "Сетевой адрес веб-сервера")
+    dsn := flag.String("dsn", "web:pass@/snippetbox?parseTime=true", "Название MySQL источника данных")
+    flag.Parse()
+
+    infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+    errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+    db, err := openDB(*dsn)
+    if err != nil {
+        errorLog.Fatal(err)
+    }
+    defer db.Close()
+
+    // Инициализируем новый кэш шаблона...
+    templateCache, err := newTemplateCache("./ui/html/")
+    if err != nil {
+        errorLog.Fatal(err)
+    }
+
+    // И добавляем его в зависимостях нашего
+    // веб-приложения.
+    app := &application{
+        errorLog:      errorLog,
+        infoLog:       infoLog,
+        snippets:      &mysql.SnippetModel{DB: db},
+        templateCache: templateCache,
+    }
+
+    srv := &http.Server{
+        Addr:     *addr,
+        ErrorLog: errorLog,
+        Handler:  app.routes(),
+    }
+
+    infoLog.Printf("Запуск сервера на http://127.0.0.1%s", *addr)
+    err = srv.ListenAndServe()
+    errorLog.Fatal(err)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+    db, err := sql.Open("mysql", dsn)
+    if err != nil {
+        return nil, err
+    }
+    if err = db.Ping(); err != nil {
+        return nil, err
+    }
+    return db, nil
+}
